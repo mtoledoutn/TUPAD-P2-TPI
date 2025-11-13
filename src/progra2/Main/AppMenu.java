f822d383cf693a456604f15edd8bec35a7ce01b4
mtoledoDL <mtoledo@directoriolegislativo.org>
2025-11-13 18:43:08 -0300

    add app menu

--- src/progra2/Main/AppMenu.java
+++ src/progra2/Main/AppMenu.java
@@ -1,8 +1,64 @@
 package progra2.Main;
 
+import java.util.Scanner;
+import progra2.DAO.FichaBibliograficaDAO;
+import progra2.DAO.LibroDAO;
+import progra2.Service.FichaBibliograficaService;
+import progra2.Service.LibroService;
+
 /** JAVADOC AQUÍ */
 public class AppMenu {
     
-    // Código aquí
+    private final Scanner scanner;
+    
+    private final MenuHandler menuHandler;
+    
+    private boolean running;
+    
+    public AppMenu() {
+        this.scanner = new Scanner(System.in);
+        LibroService libroService = createLibroService();
+        this.menuHandler = new MenuHandler(scanner, libroService);
+        this.running = true;
+    }
+    
+    public static void main(String[] args) {
+        AppMenu app = new AppMenu();
+        app.run();
+    }
+    
+    public void run() {
+        try (scanner) {
+            while (running) {
+                try {
+                    MenuDisplay.mostrarMenuPrincipal();
+                    int opcion = Integer.parseInt(scanner.nextLine());
+                    processOption(opcion);
+                } catch (NumberFormatException e) {
+                    System.out.println("Entrada invalida. Por favor, ingrese un numero.");
+                }
+            }
+        }
+    }
+    
+    private void processOption(int opcion) {
+        switch (opcion) {
+            case 1 -> menuHandler.crearLibro();
+            case 2 -> menuHandler.listarLibros();
+            case 3 -> menuHandler.actualizarLibro();
+            case 0 -> {
+                System.out.println("Saliendo...");
+                running = false;
+            }
+            default -> System.out.println("Opcion no valida.");
+        }
+    }
+    
+    private LibroService createLibroService() {
+        FichaBibliograficaDAO fichaBibliograficaDAO = new FichaBibliograficaDAO();
+        LibroDAO libroDAO = new LibroDAO();
+        FichaBibliograficaService fichaBibliograficaService = new FichaBibliograficaService(fichaBibliograficaDAO);
+        return new LibroService(libroDAO, fichaBibliograficaService);
+    }
     
 }
--- src/progra2/Main/Main.java
+++ src/progra2/Main/Main.java
@@ -2,10 +2,8 @@ package progra2.Main;
 
 public class Main {
     
-    public static void main(String[] args) {
-        
-        // TODO code application logic here
-        
+    public static void main(String[] args) {   
+        AppMenu app = new AppMenu();
+        app.run();
     }
-    
 }
